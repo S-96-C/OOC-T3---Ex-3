cf0b283839963870910b43ab32342695a55be3ac
Shehan Cooray <109017782+S-96-C@users.noreply.github.com>
2022-07-14 08:40:55 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+struct Box{
+  int height;
+  int width;
+  int length;
+};
+
+int volume(Box b);
+
+int main() {
+
+    Box box1;
+    Box box2;
+
+    int totalVolume;
+
+    cout << "Enter Box 1 Height : ";
+    cin >> box1.height;
+    cout << "Enter Box 1 Width : ";
+    cin >> box1.width;
+    cout << "Enter Box 1 Length : ";
+    cin >> box1.length;
+
+    cout << "Enter Box 2 Height : ";
+    cin >> box2.height;
+    cout << "Enter Box 2 Width : ";
+    cin >> box2.width;
+    cout << "Enter Box 2 Length : ";
+    cin >> box2.length;
+
+    totalVolume = volume(box1) + volume(box2);
+
+    cout << "Volume of Box is " << totalVolume << endl;
+    return 0;
+}
+
+int volume(Box b) {
+  int volume = b.height * b.width * b.length;
+  return volume;
+
+}
